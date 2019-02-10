772857da7a3a063d68ffe0a191d6fab5a3e31d36
schandramouli2 <subbiah_chandramouli@apple.com>
2019-02-09 22:06:24 -0800

    added material components

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <md-tabs>
+    <md-tab md-label="Home" md-icon="home"></md-tab>
+    <md-tab md-label="Pages" md-icon="pages"></md-tab>
+    <md-tab md-label="Posts" md-icon="add_to_home_screen"></md-tab>
+    <md-tab md-label="Favorites" md-icon="favorite"></md-tab>
+  </md-tabs>
+</template>
+
+<script>
+
+export default {
+  name: 'Home'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .md-tabs + .md-tabs {
+    margin-top: 24px;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueMaterial from 'vue-material'
+import Vuetify from 'vuetify'
 
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default-dark.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 Vue.config.productionTip = false
+Vue.use(VueMaterial)
+Vue.use(Vuetify, {
+  iconfont: 'md' // 'md' || 'mdi' || 'fa' || 'fa4'
+})
 
 /* eslint-disable no-new */
 new Vue({
